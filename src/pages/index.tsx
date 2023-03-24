49736e1fa5ef780e86dbc1b6f573e4f3bcc4ea74
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2023-03-24 14:20:09 +0100

    Add connect buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,57 @@
+import { useEthers } from "@usedapp/core";
+
 export default function Home() {
   return (
     <>
       <main>
         <p>Let&apos;s get started</p>
+        <ConnectButton />
       </main>
     </>
   );
 }
+
+const ConnectButton = () => {
+  const { account, deactivate, activateBrowserWallet } = useEthers();
+
+  const metamaskActivate = () => activateBrowserWallet({ type: "Metamask" });
+  const walletConnectActivate = () =>
+    activateBrowserWallet({ type: "WalletConnect" });
+
+  const ConnectButtons = () => (
+    <div>
+      <div>
+        <button
+          onClick={metamaskActivate}
+        >{`Connect with \"Metamask\"`}</button>
+      </div>
+      <div>
+        <button
+          onClick={walletConnectActivate}
+        >{`Connect with \"Wallet Connect\"`}</button>
+      </div>
+    </div>
+  );
+
+  const WalletConnectConnect = () => (
+    <div>
+      {account && (
+        <div>
+          <div className="inline">
+            <div className="account">{account}</div>
+          </div>
+          <br />
+        </div>
+      )}
+      {!account && <ConnectButtons />}
+      {account && <button onClick={deactivate}>Disconnect</button>}
+      <br />
+    </div>
+  );
+
+  return (
+    <div>
+      <WalletConnectConnect />
+    </div>
+  );
+};
