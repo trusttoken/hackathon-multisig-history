ae39fccd5bdbca62755af45bd1d6257869fa6008
Maria Rushkova <maria.rushkova@trusttoken.com>
2023-03-24 16:27:12 +0100

    Add styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,2 +1,6 @@
 export * from "./globalStyles";
 export * from "./theme";
+export * from "./borderRadiuses";
+export * from "./zIndexes";
+export * from "./shadows";
+export * from "./transitions";
---
+++ src/styles/shadows.ts
@@ -0,0 +1,7 @@
+export const shadows = {
+  xs: "0px 1px 2px rgba(0, 0, 0, 0.06), 0px 1px 3px rgba(0, 0, 0, 0.1)",
+  s: "0px 2px 4px rgba(0, 0, 0, 0.06), 0px 4px 6px rgba(0, 0, 0, 0.1)",
+  m: "0px 10px 15px rgba(0, 0, 0, 0.1), 0px 4px 6px rgba(0, 0, 0, 0.05)",
+  l: "0px 20px 25px rgba(0, 0, 0, 0.1), 0px 10px 10px rgba(0, 0, 0, 0.04)",
+  xl: "40px 25px 50px rgba(0, 0, 0, 0.25)",
+};
