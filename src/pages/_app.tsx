2621c1f4d786c943f1b68246e10753cb95d39dc9
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2023-03-24 12:03:41 +0100

    🧹 Add UseDapp (#2)
    * Update gitignore
    
    * Add query client
    
    * Add usedapp
    
    * Update pnpm lock

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {DAppProvider} from "@usedapp/core";
+import {getDAppConfig} from "@/pages/providers/usedapp";
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <QueryClientProvider client={queryClient}>
+    <DAppProvider config={getDAppConfig()}>
+      <Component {...pageProps} />
+    </DAppProvider>
+  </QueryClientProvider>
 }
