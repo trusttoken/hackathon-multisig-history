799b1751cd87800484c101758172c9790ca0a32c
Maria Rushkova <maria.rushkova@trusttoken.com>
2023-03-24 12:39:10 +0100

    Fix lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import {useBlockNumber, useEthers} from "@usedapp/core";
+import { useBlockNumber, useEthers } from "@usedapp/core";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const blockNumber =useBlockNumber()
-  const {chainId} = useEthers()
-  console.log('chainId: ', chainId)
-  console.log('blockNumber: ', blockNumber)
+  const blockNumber = useBlockNumber();
+  const { chainId } = useEthers();
+  console.log("chainId: ", chainId);
+  console.log("blockNumber: ", blockNumber);
   return (
     <>
       <Head>
