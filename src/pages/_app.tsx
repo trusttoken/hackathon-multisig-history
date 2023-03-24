bb02624751c3a138cddeb7a773dad6eaef589376
Maria Rushkova <66270386+mrushkova@users.noreply.github.com>
2023-03-24 12:46:42 +0100

    🌈 Add basic styles (#3)
    * Add `eslint` & `prettier`
    
    * Add colors and basic styles
    
    * Fix lint
    
    * Fix lint
    
    * Fix lint
    
    * Fix gitignore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {DAppProvider} from "@usedapp/core";
-import {getDAppConfig} from "@/pages/providers/usedapp";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles, theme } from "../styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { DAppProvider } from "@usedapp/core";
+import { getDAppConfig } from "@/pages/providers/usedapp";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <QueryClientProvider client={queryClient}>
-    <DAppProvider config={getDAppConfig()}>
-      <Component {...pageProps} />
-    </DAppProvider>
-  </QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <DAppProvider config={getDAppConfig()}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </DAppProvider>
+    </QueryClientProvider>
+  );
 }
