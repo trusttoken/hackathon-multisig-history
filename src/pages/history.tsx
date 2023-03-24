f15894a84efdc5bc0a46e532a2b58c156f531709
Maria Rushkova <maria.rushkova@trusttoken.com>
2023-03-24 12:44:56 +0100

    Fix lint

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,50 +1,52 @@
-import {useBlockNumber, useEthers} from "@usedapp/core";
-import {useMemo} from "react";
+import { useBlockNumber, useEthers } from "@usedapp/core";
+import { useMemo } from "react";
 import EthersAdapter from "@safe-global/safe-ethers-lib";
-import {ethers} from "ethers";
+import { ethers } from "ethers";
 import SafeServiceClient from "@safe-global/safe-service-client";
-import {useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 export default function History() {
-    const blockNumber = useBlockNumber()
-    const {library} = useEthers()
+  const blockNumber = useBlockNumber();
+  const { library } = useEthers();
 
-    const serviceClient = useMemo(() => {
-        if (library === undefined) {
-            return undefined
-        }
-        const ethAdapter = new EthersAdapter({
-            ethers,
-            signerOrProvider: library
-        })
+  const serviceClient = useMemo(() => {
+    if (library === undefined) {
+      return undefined;
+    }
+    const ethAdapter = new EthersAdapter({
+      ethers,
+      signerOrProvider: library,
+    });
 
-        return new SafeServiceClient({
-            txServiceUrl: 'https://safe-transaction-goerli.safe.global',
-            ethAdapter
-        })
-    }, [library])
+    return new SafeServiceClient({
+      txServiceUrl: "https://safe-transaction-goerli.safe.global",
+      ethAdapter,
+    });
+  }, [library]);
 
-    const {data: safes} = useQuery({
-        queryKey: ['safes', blockNumber],
-        queryFn: async () => {
-            return serviceClient?.getSafesByOwner('0xe13610d0a3e4303c70791773C5DF8Bb16de185d1')
-        }
-    })
+  const { data: safes } = useQuery({
+    queryKey: ["safes", blockNumber],
+    queryFn: async () => {
+      return serviceClient?.getSafesByOwner(
+        "0xe13610d0a3e4303c70791773C5DF8Bb16de185d1"
+      );
+    },
+  });
 
-    const {data: multisigTxs} = useQuery({
-        queryKey: ['txs', '0x10443C6e07D43ad15D749931379feC963fCb6baD'],
-        queryFn: async () =>{
-            return serviceClient?.getMultisigTransactions('0x10443C6e07D43ad15D749931379feC963fCb6baD')
-        }
-    })
-    console.log('multisigTxs: ', multisigTxs)
+  const { data: multisigTxs } = useQuery({
+    queryKey: ["txs", "0x10443C6e07D43ad15D749931379feC963fCb6baD"],
+    queryFn: async () => {
+      return serviceClient?.getMultisigTransactions(
+        "0x10443C6e07D43ad15D749931379feC963fCb6baD"
+      );
+    },
+  });
+  console.log("multisigTxs: ", multisigTxs);
 
-    return <div>
-        <pre>
-            {JSON.stringify(safes)}
-        </pre>
-        <pre>
-            {JSON.stringify(multisigTxs)}
-        </pre>
+  return (
+    <div>
+      <pre>{JSON.stringify(safes)}</pre>
+      <pre>{JSON.stringify(multisigTxs)}</pre>
     </div>
+  );
 }
